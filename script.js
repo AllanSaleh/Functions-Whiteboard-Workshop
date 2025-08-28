eda922b2b8d415c53c4e7840118ad30c24d7f85b
allan <allansaleh95@gmail.com>
2025-08-28 04:40:48 -0400

    JavaScript Functions Whiteboard Workshop Part 3

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // DRY - Don't Repeat Yourself
 
-function greet(name = 'Ben', topic = 'Java') {
-  // console.log("Hello " + name + "!");
-  console.log(`Hello, ${name}!`);
-  console.log('Welcome to the class');
-  console.log(`We are learning about functions in ${topic}!`);
-}
+// function greet(name = 'Ben', topic = 'Java') {
+//   // console.log("Hello " + name + "!");
+//   console.log(`Hello, ${name}!`);
+//   console.log('Welcome to the class');
+//   console.log(`We are learning about functions in ${topic}!`);
+// }
 
-greet('Allan', 'JavaScript');
+// greet('Allan', 'JavaScript');
 
-greet('Tom', 'Python');
-greet();
+// greet('Tom', 'Python');
+// greet();
 
 // logging prints are for the developer to confirm things are working
 // returning gives a value back to the program so that we can use it later
@@ -101,9 +101,6 @@ console.log(maxNumber(15, 23));
 const max = (num1, num2) => (num1 > num2 ? num1 : num2);
 console.log(max(12, 5));
 
-
-
-
 // Using multiple functions together
 function add(a, b) {
   return a + b;
@@ -113,14 +110,13 @@ function doubleSum(a, b) {
   return add(a, b) * 2;
 }
 
-console.log(doubleSum(3, 4)); 
+console.log(doubleSum(3, 4));
 
 const average = (num1, num2) => {
-  return add(num1,num2)/2;
-}
+  return add(num1, num2) / 2;
+};
 
-console.log("The average is",average(5,15))
-
+console.log('The average is', average(5, 15));
 
 // Example 2
 // const isAdult = (age) => {
@@ -131,17 +127,125 @@ console.log("The average is",average(5,15))
 //   }
 // }
 
+const isAdult = (age) => age >= 18;
 
-const isAdult = age => age >= 18;
-
-function canEnterClub(age){
-  if(isAdult(age)){
-    return "Welcome in!"
+function canEnterClub(age) {
+  if (isAdult(age)) {
+    return 'Welcome in!';
   } else {
-    return "Sorry, not old enough."
+    return 'Sorry, not old enough.';
   }
 }
 
-console.log(canEnterClub(17))
+console.log(canEnterClub(17));
 
+// Functions are first class citizens
 
+function sayHi() {
+  console.log('Hi!');
+}
+
+const greet = sayHi; // assign function to a variable
+greet(); // run sayHi function
+
+// Since functions are values, we can pass them into other functions. These are called callbacks
+function doTwice(action) {
+  action();
+  action();
+
+  // for (let i = 0; i < 2; i++) {
+  //   action();
+  // }
+}
+
+function runActionNTimes(times, action) {
+  for (let i = 0; i < times; i++) {
+    action();
+  }
+}
+
+function laugh() {
+  console.log('😂');
+}
+
+// doTwice(laugh)
+
+runActionNTimes(8, laugh);
+runActionNTimes(3, laugh);
+
+// You can make a function return another function
+
+function makeMultiplier(factor) {
+  return function (num) {
+    return num * factor;
+  };
+}
+
+const double2 = makeMultiplier(2);
+
+const cube = makeMultiplier(3);
+
+console.log(double2(5));
+console.log(cube(5));
+
+// Built-in callbacks
+// foreach
+// map
+// filter
+
+const numbers = [1, 2, 3, 4, 5, 6];
+
+// numbers.forEach(function (num) {
+//   console.log('number: ', num);
+// });
+numbers.forEach((num) => console.log('number: ', num));
+
+// map function: Calls a defined callback function on each element of an array, and returns an array that contains the results.
+
+const doubled = numbers.map((num) => num * 2);
+
+console.log('New Doubled array:', doubled);
+
+// filter()
+
+// from our numbers array, I want the even numbers
+
+const evens = numbers.filter((num) => num % 2 == 0);
+
+console.log('The evens of the numbers:', evens);
+
+// Challenge 1
+// Make a new array where every name is lowercased
+// use map function
+const names = ['Ben', 'Sally', 'AARON'];
+
+// Challenge 2
+// Filter out names shorter than 5 letters
+
+// use loops inside functions
+const printNumbersUpTo = (number) => {
+  for (let i = 0; i <= number; i++) {
+    console.log(i);
+  }
+};
+
+printNumbersUpTo(5);
+
+printNumbersUpTo(12);
+
+// Pseudocode
+const sumArray = (numbersArr) => {
+  // 1. Create a variable called result = 0
+  let result = 0;
+  // 2. Loop over every element in the array
+  for (let i = 0; i < numbersArr.length; i++) {
+    // 3. add that element to the result
+    // result = result + numbersArr[i]
+    result += numbersArr[i];
+  }
+  // 4. return the result variable
+  return result;
+};
+
+const nums = [2, 5, 6, 8];
+console.log(sumArray(nums));
